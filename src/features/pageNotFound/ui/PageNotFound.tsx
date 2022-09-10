59501529ea9f80257db7326fba1053d47570b54a
SPK80 <psukonkin@gmail.com>
2022-09-10 19:15:51 +0300

    feat: update PageNotFound

--- src/features/pageNotFound/ui/PageNotFound.tsx
+++ src/features/pageNotFound/ui/PageNotFound.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import s from "./pageNotFound.module.scss"
 
-export const PageNotFound: React.FC = () =>
+type PropsType = {}
+
+export const PageNotFound: React.FC<PropsType> = ({}) =>
     <div className={s.pageNotFound}>
         <div>404</div>
         <div>Page not found!</div>
