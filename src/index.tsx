3d927d9c7f194be4aeb933b3c484bcb422910f05
SPK80 <psukonkin@gmail.com>
2022-09-10 10:13:54 +0300

    refactor: create appReducer, move store to bll folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './app/App';
+import App from './app/ui/App';
 import reportWebVitals from './reportWebVitals';
-import {HashRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import {store} from "./app/store/store";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
-);
+)
 
-root.render(
-    <HashRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </HashRouter>
-);
-
-reportWebVitals();
+root.render(<App/>)
+reportWebVitals()
