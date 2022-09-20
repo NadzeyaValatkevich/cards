2e8787275682ca110e1fecd68d3c0d7d1f71c8aa
SPK80 <psukonkin@gmail.com>
2022-09-20 19:43:51 +0300

    feat: add sx to ContentWrapper props

--- src/common/components/contentWrapper/ContentWrapper.tsx
+++ src/common/components/contentWrapper/ContentWrapper.tsx
@@ -2,12 +2,15 @@ import React, { ReactNode } from 'react'
 
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
+import { Theme } from '@mui/material/styles'
+import { SxProps } from '@mui/system'
 
 type PropsType = {
+  sx?: SxProps<Theme>
   children: ReactNode
 }
 
-export const ContentWrapper: React.FC<PropsType> = ({ children }) => (
+export const ContentWrapper: React.FC<PropsType> = ({ children, sx }) => (
   <Box
     sx={{
       display: 'flex',
@@ -18,6 +21,7 @@ export const ContentWrapper: React.FC<PropsType> = ({ children }) => (
       sx={{
         marginTop: '60px',
         padding: '10px',
+        ...sx,
       }}
     >
       {children}
