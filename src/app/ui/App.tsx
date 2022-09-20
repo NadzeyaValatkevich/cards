c6dc5c88e62cdcf3ca203b297b924833f00e2e74
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-09-20 23:49:59 +0300

    feat: added <ThemeProvider/> for global theming

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
 
 import './App.css'
+import { ThemeProvider } from '@mui/material'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 
 import { ContentWrapper } from '../../common/components/contentWrapper/ContentWrapper'
+import { theme } from '../../common/styles/theme'
 import { Header } from '../../features/header/Header'
 import { store } from '../bll/store'
 
 import { RoutesComponent } from './RoutesComponent'
 
 const App = () => (
-  <HashRouter>
-    <Provider store={store}>
-      <div className="App">
-        <Header />
-        <ContentWrapper>
-          <RoutesComponent />
-        </ContentWrapper>
-      </div>
-    </Provider>
-  </HashRouter>
+  <ThemeProvider theme={theme}>
+    <HashRouter>
+      <Provider store={store}>
+        <div className="App">
+          <Header />
+          <ContentWrapper>
+            <RoutesComponent />
+          </ContentWrapper>
+        </div>
+      </Provider>
+    </HashRouter>
+  </ThemeProvider>
 )
 
 export default App
