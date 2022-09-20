b6d14ea76532cc8803c6801e254e78fdde2e0bf4
SPK80 <psukonkin@gmail.com>
2022-09-20 21:22:51 +0300

    feat: implement SignIn layout

--- src/features/auth/ui/SignIn.tsx
+++ src/features/auth/ui/SignIn.tsx
@@ -1,7 +1,64 @@
 import React from 'react'
 
-type PropsType = {}
+import { Button, Checkbox, FormControlLabel, Link, TextField, Typography } from '@mui/material'
 
-export const SignIn: React.FC<PropsType> = ({}) => {
-  return <div>SignIn</div>
+import { ContentWrapper } from '../../../common/components/contentWrapper/ContentWrapper'
+
+export const SignIn: React.FC = () => {
+  return (
+    <ContentWrapper
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '35px 33px',
+      }}
+    >
+      <Typography variant="h4">Sign In</Typography>
+      <form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: '30px',
+          width: '347px',
+        }}
+        onSubmit={() => alert('saved')}
+      >
+        <TextField
+          style={{ width: '100%', marginBottom: '15px', height: '1' }}
+          type="email"
+          label="Email"
+          variant="standard"
+        />
+        <TextField
+          style={{ width: '100%', marginBottom: '15px', height: '1' }}
+          type="password"
+          label="Password"
+          variant="standard"
+        />
+        <FormControlLabel
+          style={{ alignSelf: 'flex-start', marginBottom: '29px' }}
+          label="Remember me"
+          control={<Checkbox defaultChecked />}
+        />
+        <Link style={{ alignSelf: 'flex-end', marginBottom: '99px' }} href="#" variant="body2">
+          Forgot Password?
+        </Link>
+        <Button
+          style={{ width: '100%', marginBottom: '31px', borderRadius: '20px' }}
+          variant="contained"
+          color="primary"
+        >
+          Sign In
+        </Button>
+        <Typography style={{ marginBottom: '11px' }} variant="subtitle2">
+          Already have an account?
+        </Typography>
+        <Link href="#" variant="body2">
+          Sign Up
+        </Link>
+      </form>
+    </ContentWrapper>
+  )
 }
