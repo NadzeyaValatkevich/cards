cf5c07f6e078420ea8b52c33541cb28c7b6aeec6
SPK80 <psukonkin@gmail.com>
2022-09-22 16:54:38 +0300

    refactor: change ContentWrapper layout using react-hook-form-mui

--- src/features/auth/ui/SignIn.tsx
+++ src/features/auth/ui/SignIn.tsx
@@ -1,70 +1,64 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import VisibilityIcon from '@mui/icons-material/Visibility'
-import VisibilityOffIcon from '@mui/icons-material/VisibilityOff'
-import { Button, Checkbox, FormControlLabel, Link, TextField, Typography } from '@mui/material'
+import { Button, Link, Typography } from '@mui/material'
+import {
+  CheckboxElement,
+  FormContainer,
+  PasswordElement,
+  TextFieldElement,
+} from 'react-hook-form-mui'
 
 import { ContentWrapper } from '../../../common/components/contentWrapper/ContentWrapper'
 
 export const SignIn: React.FC = () => {
-  const [showPassword, setShowPassword] = useState(false)
-
   return (
-    <ContentWrapper>
+    <ContentWrapper sx={{ width: '450px' }}>
       <Typography variant="h4">Sign In</Typography>
-      <form
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          marginTop: '30px',
-          width: '347px',
-        }}
-        onSubmit={() => alert('saved')}
-      >
-        <TextField
-          style={{ width: '100%', marginBottom: '15px', height: '1' }}
+      <FormContainer defaultValues={{}} FormProps={{}}>
+        <TextFieldElement
+          required
           type="email"
           label="Email"
           variant="standard"
+          margin="dense"
+          name="email"
+          sx={{ width: '100%' }}
         />
-        <div style={{ display: 'flex', width: '100%' }}>
-          <TextField
-            style={{ width: '100%', marginBottom: '15px', height: '1' }}
-            type={showPassword ? 'text' : 'password'}
-            label="Password"
-            variant="standard"
-          />
-          <Checkbox
-            checked={showPassword}
-            onChange={e => setShowPassword(e.currentTarget.checked)}
-            icon={<VisibilityIcon />}
-            checkedIcon={<VisibilityOffIcon />}
-          />
-        </div>
-
-        <FormControlLabel
-          style={{ alignSelf: 'flex-start', marginBottom: '29px' }}
-          label="Remember me"
-          control={<Checkbox defaultChecked />}
+        <PasswordElement
+          required
+          margin={'dense'}
+          label={'Password'}
+          name={'password'}
+          variant={'standard'}
+          sx={{ width: '100%', marginBottom: '30px' }}
         />
-        <Link style={{ alignSelf: 'flex-end', marginBottom: '99px' }} href="#" variant="body2">
+        <CheckboxElement
+          required
+          name={'rememberMe'}
+          label={'Remember me'}
+          sx={{ marginBottom: '30px' }}
+        />
+        <Link
+          sx={{ display: 'block', textAlign: 'end', marginBottom: '50px' }}
+          href="#"
+          variant="body2"
+        >
           Forgot Password?
         </Link>
         <Button
-          style={{ width: '100%', marginBottom: '31px', borderRadius: '20px' }}
+          sx={{ width: '100%', marginBottom: '31px', borderRadius: '20px' }}
           variant="contained"
           color="primary"
         >
           Sign In
         </Button>
-        <Typography style={{ marginBottom: '11px' }} variant="subtitle2">
-          Already have an account?
-        </Typography>
-        <Link href="#" variant="body2">
-          Sign Up
-        </Link>
-      </form>
+      </FormContainer>
+      <Typography sx={{ marginBottom: '11px' }} variant="subtitle2">
+        Already have an account?
+      </Typography>
+      <Link href="#" variant="body2">
+        Sign Up
+      </Link>
     </ContentWrapper>
   )
 }
