202d09dd7b1103c74684eae7e3aa927a7a32b827
SPK80 <psukonkin@gmail.com>
2022-09-22 13:43:17 +0300

    refactor: add sx to ContentWrapper props

--- src/common/components/contentWrapper/ContentWrapper.tsx
+++ src/common/components/contentWrapper/ContentWrapper.tsx
@@ -2,12 +2,15 @@ import React, { ReactNode } from 'react'
 
 import { Paper } from '@mui/material'
 import Container from '@mui/material/Container'
+import { Theme } from '@mui/material/styles'
+import { SxProps } from '@mui/system'
 
 type PropsType = {
+  sx?: SxProps<Theme>
   children: ReactNode
 }
 
-export const ContentWrapper: React.FC<PropsType> = ({ children }) => (
+export const ContentWrapper: React.FC<PropsType> = ({ children, sx }) => (
   <Container
     sx={{
       display: 'flex',
@@ -16,6 +19,7 @@ export const ContentWrapper: React.FC<PropsType> = ({ children }) => (
       alignItems: 'center',
       height: '100%',
       marginTop: '20px',
+      ...sx,
     }}
   >
     <Paper
