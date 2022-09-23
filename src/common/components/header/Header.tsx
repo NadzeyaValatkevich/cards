6e07d31b92a419ad44bfc8f765cd6fbac36a94be
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-09-23 18:13:37 +0300

    feat: add redirect to signIn page

--- src/common/components/header/Header.tsx
+++ src/common/components/header/Header.tsx
@@ -5,16 +5,23 @@ import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
 import Toolbar from '@mui/material/Toolbar'
+import { useNavigate } from 'react-router-dom'
 
-import logo from '../../assets/image/logo.svg'
+import { SIGN_IN } from 'app/ui/RoutesComponent'
+import logo from 'common/assets/image/logo.svg'
 
 export const Header = () => {
+  const navigate = useNavigate()
+  const signInOnClickHandler = () => {
+    navigate(SIGN_IN)
+  }
+
   return (
     <AppBar position="static" sx={{ bgcolor: '#FCFCFC' }} elevation={2}>
       <Container maxWidth="md">
         <Toolbar disableGutters sx={{ justifyContent: 'space-between' }}>
           <Box component={'img'} src={logo} alt={'It-incubator logo'} />
-          <Button variant={'contained'} color={'primary'}>
+          <Button variant={'contained'} color={'primary'} onClick={signInOnClickHandler}>
             Sign in
           </Button>
         </Toolbar>
