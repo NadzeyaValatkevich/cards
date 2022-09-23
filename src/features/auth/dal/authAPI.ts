96fbc2c5433a284f41d4262d9e65e7248f842ece
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-09-24 01:23:00 +0300

    refactor: change type from loginReturnType to ProfileStateType

--- src/features/auth/dal/authAPI.ts
+++ src/features/auth/dal/authAPI.ts
@@ -1,21 +1,8 @@
+import { ProfileStateType } from '../../profile/bll/profileReducer'
 import { LoginType } from '../ui/SignIn_ver2'
 import { registerType } from '../ui/SignUp'
 
 import { instance } from 'app/dal/instance'
-
-type loginReturnType = {
-  _id: string | null
-  email: string | null
-  name: string | null
-  avatar?: string | null
-  publicCardPacksCount: number | null
-  created: Date | null
-  updated: Date | null
-  isAdmin: boolean | null
-  verified: boolean | null
-  rememberMe: boolean | null
-  error?: string | null
-}
 
 type registerReturnType = {
   addedUser: {}
@@ -29,13 +16,13 @@ type logoutReturnType = {
 
 export const authAPI = {
   me() {
-    return instance.post<loginReturnType>('auth/me')
+    return instance.post<ProfileStateType>('auth/me')
   },
   register(data: registerType) {
     return instance.post<registerReturnType>('auth/register', data)
   },
   login(data: LoginType) {
-    return instance.post<loginReturnType>('auth/login', data)
+    return instance.post<ProfileStateType>('auth/login', data)
   },
   logout() {
     return instance.delete<logoutReturnType>('auth/me')
