dfc6c6fa6962da5eeb4510c104ec31200bb63122
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-09-23 17:40:47 +0300

    refactor: delete method 'logout' and change name 'updateTitle' to 'updateProfile'

--- src/features/profile/dal/profileAPI.ts
+++ src/features/profile/dal/profileAPI.ts
@@ -1,12 +1,10 @@
-import { instance } from '../../../app/dal/instance'
 import { updateProfileType } from '../bll/profileActions'
 import { ProfileStateType } from '../bll/profileReducer'
 
+import { instance } from 'app/dal/instance'
+
 export const profileAPI = {
-  logout() {
-    return instance.delete('auth/me')
-  },
-  updateTitle({ name }: updateProfileType) {
-    return instance.put<ProfileStateType, updateProfileType>('/auth/me', { name })
+  updateProfile({ name }: updateProfileType) {
+    return instance.put<ProfileStateType, updateProfileType>('auth/me', { name })
   },
 }
