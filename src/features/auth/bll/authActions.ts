26de2b77587142664b1c3cb8e63f2e32feeaddee
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-09-23 17:30:27 +0300

    feat: add 2 AC: setIsLoggedInAC, setIsRegisteredAC

--- src/features/auth/bll/authActions.ts
+++ src/features/auth/bll/authActions.ts
@@ -1,3 +1,9 @@
-export type AuthActionsType = ReturnType<typeof fakeAC>
+export type AuthActionsType =
+  | ReturnType<typeof setIsLoggedInAC>
+  | ReturnType<typeof setIsRegisteredAC>
 
-export const fakeAC = () => ({ type: 'FAKE-ACTION' } as const)
+export const setIsLoggedInAC = (value: boolean) =>
+  ({ type: 'login/SET-IS-LOGGED-IN', payload: { value } } as const)
+
+export const setIsRegisteredAC = (value: boolean) =>
+  ({ type: 'login/SET-IS-REGISTERED', payload: { value } } as const)
