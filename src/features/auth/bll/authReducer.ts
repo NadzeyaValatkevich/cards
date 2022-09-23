d2ea767825cd1f36361f5c229998644750a8df1c
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-09-23 17:33:47 +0300

    feat: add 2 cases: login/SET-IS-LOGGED-IN, login/SET-IS-REGISTERED

--- src/features/auth/bll/authReducer.ts
+++ src/features/auth/bll/authReducer.ts
@@ -1,14 +1,24 @@
 import { AuthActionsType } from './authActions'
 
-export type AuthStateType = {}
+export type AuthStateType = {
+  isLoggedIn: boolean
+  isRegistered: boolean
+}
 
-const initialState: AuthStateType = {}
+const initialState: AuthStateType = {
+  isLoggedIn: false,
+  isRegistered: false,
+}
 
 export const authReducer = (
   state: AuthStateType = initialState,
   action: AuthActionsType
 ): AuthStateType => {
   switch (action.type) {
+    case 'login/SET-IS-LOGGED-IN':
+      return { ...state, isLoggedIn: action.payload.value }
+    case 'login/SET-IS-REGISTERED':
+      return { ...state, isRegistered: action.payload.value }
     default:
       return state
   }
