f6ffd496cf796f044ffcf0ebf9cc00a784756b90
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-10-02 18:04:22 +0300

    refactor: added check for availability token at state. If true then nothing if false producing request auth/me

--- src/app/bll/appThunks.ts
+++ src/app/bll/appThunks.ts
@@ -1,26 +1,30 @@
-import { setIsLoggedInAC } from '../../features/auth/bll/authActions'
-import { setProfileAC } from '../../features/profile/bll/profileActions'
-
 import { setAppInfoAC, setAppInitializedAC, setAppStatusAC } from './appActions'
 import { RequestStatusType } from './appReducer'
 import { AppThunk } from './store'
 
 import { errorUtils } from 'common/utils/error-utils'
+import { setIsLoggedInAC } from 'features/auth/bll/authActions'
 import { authAPI } from 'features/auth/dal/authAPI'
+import { setProfileAC } from 'features/profile/bll/profileActions'
 import { ProfileStateType } from 'features/profile/bll/profileReducer'
 
-export const initTC = (): AppThunk => async dispatch => {
-  dispatch(setAppStatusAC(RequestStatusType.loading))
-  try {
-    const res = await authAPI.me()
+export const initTC = (): AppThunk => async (dispatch, getState) => {
+  const token = getState().profile.token
 
-    dispatch(setProfileAC(res.data as ProfileStateType))
-    dispatch(setIsLoggedInAC(true))
-    dispatch(setAppStatusAC(RequestStatusType.succeeded))
-    dispatch(setAppInfoAC('You are logged in'))
-  } catch (error: any) {
-    errorUtils(error, dispatch)
-  } finally {
-    dispatch(setAppInitializedAC(true))
+  if (!token) {
+    dispatch(setAppStatusAC(RequestStatusType.loading))
+
+    try {
+      const res = await authAPI.me()
+
+      dispatch(setProfileAC(res.data as ProfileStateType))
+      dispatch(setIsLoggedInAC(true))
+      dispatch(setAppStatusAC(RequestStatusType.succeeded))
+      dispatch(setAppInfoAC('You are logged in'))
+    } catch (error: any) {
+      errorUtils(error, dispatch)
+    } finally {
+      dispatch(setAppInitializedAC(true))
+    }
   }
 }
