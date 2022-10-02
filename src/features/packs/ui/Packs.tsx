84258f733db649a8f6b08a4bcafa148e560f0e62
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-10-02 18:13:24 +0300

    refactor: added to PacksComponent PacksTableComponent with additional functionality

--- src/features/packs/ui/Packs.tsx
+++ src/features/packs/ui/Packs.tsx
@@ -1,58 +1,75 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { Column, TableInstance } from 'react-table'
+import Box from '@mui/material/Box'
+import { useSearchParams } from 'react-router-dom'
+import { Column } from 'react-table'
 
-import { getPacksTC } from '../bll/packsThunks'
-import { PackType } from '../dal/packsAPI'
+import { setPacksParamsAC } from '../bll/packsActions'
+import { initialPackParams } from '../bll/packsReducer'
+import { addPackTC, getPacksTC } from '../bll/packsThunks'
+import { AddPackDataType, PackType } from '../dal/packsAPI'
 
-import { ContentWrapper } from 'common/components/contentWrapper/ContentWrapper'
-import { PaginationPropsType } from 'common/components/Pagination/Pagination'
+import { PacksTable } from './PacksTable/PacksTable'
+
+import { RequestStatusType } from 'app/bll/appReducer'
+import { Pagination, PaginationPropsType } from 'common/components/Pagination/Pagination'
+import { sortDir } from 'common/enums/enums'
+import { ContentWrapper } from 'common/HOCs/ContentWrapper/ContentWrapper'
 import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
+import { dateParser } from 'common/utils/dateParser'
+import { compareObj } from 'common/utils/removeEmptyObj'
+import { HeaderPacksPage } from 'features/packs/ui/HeaderPacksPage/HeaderPacksPage'
+import { ToolbarTable } from 'features/packs/ui/PacksTable/PacksToolbarTable/ToolbarTable'
 
-type PacksTableDataType = PackType & {
-  actions: any
-}
-
-const columns: Column<PacksTableDataType>[] = [
+const columns: Column<PackType>[] = [
   {
     Header: 'Name',
     accessor: 'name',
-    aggregate: 'count',
-    filter: 'fuzzyText',
-    // Aggregated: ({ cell: { value } }: CellProps<PersonData>) => `${value} Names`,
+    defaultCanSort: true,
+    width: 250,
   },
   {
     Header: 'Cards',
     accessor: 'cardsCount',
-    aggregate: 'uniqueCount',
-    filter: 'numeric',
-    // Aggregated: ({ cell: { value } }: CellProps<PersonData>) => `${value} Unique Names`,
+    defaultCanSort: true,
+    width: 200,
   },
   {
     Header: 'Last Updated',
     accessor: 'updated',
-    // Aggregated: ({ cell: { value } }: CellProps<PersonData>) => `${value} Unique Names`,
+    defaultCanSort: true,
+    width: 200,
   },
   {
     Header: 'Created by',
     accessor: 'user_name',
-    aggregate: 'uniqueCount',
-    filter: 'fuzzyText',
-    // Aggregated: ({ cell: { value } }: CellProps<PersonData>) => `${value} Unique Names`,
+    defaultCanSort: true,
+    width: 200,
   },
   {
     Header: 'Actions',
-    accessor: 'actions',
-    // Aggregated: ({ cell: { value } }: CellProps<PersonData>) => `${value} Unique Names`,
+    // accessor: 'actions',
+    // Cell: <PacksActionsComponent />,
+    width: 130,
   },
 ]
+const sortDirFunc = (value: string | undefined): string => {
+  if (!value) return sortDir.asc
+
+  return value[0] === sortDir.asc ? sortDir.desc : sortDir.asc
+}
 
 export const Packs = () => {
+  //Hooks
+  const [data, setData] = useState<PackType[]>([])
   const dispatch = useAppDispatch()
-
-  const cardPacks = useAppSelector(state => state.packs.packsData.cardPacks)
-
-  const { page, pageCount, cardPacksTotalCount } = useAppSelector(state => state.packs.packsData)
+  const params = useAppSelector(state => state.packs.params)
+  const entityStatus = useAppSelector(state => state.packs.entityStatus)
+  const { page, pageCount, cardPacksTotalCount, cardPacks } = useAppSelector(
+    state => state.packs.packsData
+  )
+  const profileId = useAppSelector(state => state.profile._id)
+  const [URLSearchParams, SetURLSearchParams] = useSearchParams()
 
   const paginationProps: PaginationPropsType = {
     page,
@@ -60,46 +77,67 @@ export const Packs = () => {
     totalCount: cardPacksTotalCount,
     setParamsPacksOrCardsAC: getPacksTC,
   }
+  const sortDirection = sortDirFunc(params.sortPacks)
 
-  const [data, setData] = useState<PacksTableDataType[]>([])
+  //handlers
+  const columnSortHandler = async (e: string) => {
+    const sort = { sortPacks: `${sortDirection}${e}` }
 
-  const dummy = useCallback(
-    (instance: TableInstance<PacksTableDataType>) => () => {
-      console.log('Selected')
-    },
-    []
-  )
+    await dispatch(getPacksTC(sort))
+    SetURLSearchParams(sort)
+  }
+  const addNewPackHandler = () => {
+    const date = dateParser(new Date(Date.now()).toISOString())
+    const newPack: AddPackDataType = {
+      name: `Added new pack at ${date}`,
+      deckCover: '',
+      private: false,
+    }
 
+    dispatch(addPackTC(newPack))
+  }
+
+  //useEffect
   useEffect(() => {
     dispatch(getPacksTC())
+
+    return () => {
+      dispatch(setPacksParamsAC(initialPackParams))
+    }
   }, [])
-
   useEffect(() => {
-    // if (!data.length) {
-    // debugger
-    const tableData = cardPacks.map(
-      c =>
-        ({
-          ...c,
-          actions: '',
-        } as PacksTableDataType)
-    )
-
-    setData(tableData)
-    // }
+    setData(cardPacks)
   }, [cardPacks])
+  useEffect(() => {
+    SetURLSearchParams(compareObj(params, initialPackParams))
+  }, [params])
 
   return (
     <ContentWrapper withoutPaper>
-      {/*<Table<PacksTableDataType>*/}
-      {/*  name={'packsTable'}*/}
-      {/*  columns={columns}*/}
-      {/*  data={data || ([] as PacksTableDataType[])}*/}
-      {/*  onAdd={dummy}*/}
-      {/*  onEdit={dummy}*/}
-      {/*  onDelete={dummy}*/}
-      {/*  pagination={paginationProps}*/}
-      {/*/>*/}
+      <HeaderPacksPage
+        addNewPack={addNewPackHandler}
+        disabled={entityStatus === RequestStatusType.loading}
+      />
+      <ToolbarTable />
+      {data[0] ? (
+        <>
+          <PacksTable<PackType>
+            name={'packsTable'}
+            columns={columns}
+            data={data}
+            sortDirection={sortDirection}
+            columnSort={columnSortHandler}
+            entityStatus={entityStatus}
+            sortParam={params.sortPacks}
+            profileId={profileId}
+          />
+          <Pagination {...paginationProps} />{' '}
+        </>
+      ) : (
+        <Box>
+          {'No card packs with the entered name were found. Please change search parameters'}
+        </Box>
+      )}
     </ContentWrapper>
   )
 }
