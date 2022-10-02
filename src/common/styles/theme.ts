21d98c8af3d3e4c263ea1a95a0ca7b464f4cf1d6
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-10-02 18:11:09 +0300

    refactor: changed 'md' breakpoint from 900 to 1000

--- src/common/styles/theme.ts
+++ src/common/styles/theme.ts
@@ -37,4 +37,13 @@ export const theme = createTheme({
       },
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 1000,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
 })
