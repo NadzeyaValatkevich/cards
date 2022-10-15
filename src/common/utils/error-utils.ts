ee3be836160cdcc54a757dd51f54e3cc8246119f
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-10-15 22:03:29 +0300

    refactor: refactor error-utils

--- src/common/utils/error-utils.ts
+++ src/common/utils/error-utils.ts
@@ -1,12 +1,9 @@
 import axios, { AxiosError } from 'axios'
-import { redirect } from 'react-router-dom'
-
-import { setIsLoggedInAC } from '../../features/f0-auth/bll/authActions'
 
 import { setAppErrorAC, setAppStatusAC } from 'app/bll/appActions'
 import { RequestStatusType } from 'app/bll/appReducer'
 import { AppDispatch } from 'app/bll/store'
-import { AppRoutes } from 'common/enums/enums'
+import { setIsLoggedInAC } from 'features/f0-auth/bll/authActions'
 
 export const errorUtils = (e: AxiosError<{ error: string }>, dispatch: AppDispatch) => {
   const error = e.response?.data ? e.response.data.error : e.message
@@ -16,7 +13,6 @@ export const errorUtils = (e: AxiosError<{ error: string }>, dispatch: AppDispat
     dispatch(setAppErrorAC(error))
     if (e.response?.status === 401) {
       dispatch(setIsLoggedInAC(false))
-      // redirect(AppRoutes.SIGN_IN)
     }
   } else {
     dispatch(setAppErrorAC(`Native error ${error}`))
