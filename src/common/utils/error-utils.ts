17d1a7090b993b89a710baea3733ee24a2c6ed23
Shpura Vasiliy <shpura.vasiliy@gmail.com>
2022-10-15 19:05:21 +0300

    fix: when auth is over (status code 401) redirecting to SignIn page

--- src/common/utils/error-utils.ts
+++ src/common/utils/error-utils.ts
@@ -1,9 +1,10 @@
 import axios, { AxiosError } from 'axios'
-
-import { RequestStatusType } from '../../app/bll/appReducer'
+import { redirect } from 'react-router-dom'
 
 import { setAppErrorAC, setAppStatusAC } from 'app/bll/appActions'
+import { RequestStatusType } from 'app/bll/appReducer'
 import { AppDispatch } from 'app/bll/store'
+import { AppRoutes } from 'common/enums/enums'
 
 export const errorUtils = (e: AxiosError<{ error: string }>, dispatch: AppDispatch) => {
   const error = e.response?.data ? e.response.data.error : e.message
@@ -11,6 +12,10 @@ export const errorUtils = (e: AxiosError<{ error: string }>, dispatch: AppDispat
   dispatch(setAppStatusAC(RequestStatusType.failed))
   if (axios.isAxiosError(e)) {
     dispatch(setAppErrorAC(error))
+    console.log(e)
+    if (e.response?.status === 401) {
+      redirect(AppRoutes.SIGN_IN)
+    }
   } else {
     dispatch(setAppErrorAC(`Native error ${error}`))
   }
